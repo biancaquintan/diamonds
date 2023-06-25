b4adc8c6e34c16765fc9ada69b69ffe5cbbf0ca4
Bianca Sóta Quintãn <bsquintan@gmail.com>
2023-06-25 01:29:51 -0300

    Update and rename logic_test.rb to diamond_hunter.rb
    adjustments

--- diamond_hunter.rb
+++ diamond_hunter.rb
@@ -1,11 +1,11 @@
-def logic_test(expression)
+  def diamond_hunter(expression)
     count = 0
     string = expression.delete('.')
 
     while string.include?("<>") do
       string.length.times do |index|
         if "#{string[index]}#{string[index + 1]}" === "<>"
-          count = count + 1
+          count += 1
           string.slice!(index, 2)
         end
       end
